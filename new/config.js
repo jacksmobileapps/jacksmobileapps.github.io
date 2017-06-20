0e55a81e87332755c534f06695cbd498c285e253
Jack Flanagan <jackeverton97@gmail.com>
2017-06-20 15:29:25 -0400

    new fb site

---
+++ new/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyBqxDQ7U6BwEEiZffHxJSug4OZbOP589us",
+    authDomain: "yft3-e3ac2.firebaseapp.com",
+    databaseURL: "https://yft3-e3ac2.firebaseio.com",
+    projectId: "yft3-e3ac2",
+    storageBucket: "yft3-e3ac2.appspot.com",
+    messagingSenderId: "479122242605"
+  };
